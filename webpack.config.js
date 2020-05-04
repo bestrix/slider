fe6ec0d798dc51547818ea716278c9aeb0e6a682
f0rGetFull <f0rgetful@yandex.ru>
2020-05-04 20:23:30 +0300

    1 commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  mode:"development",
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+    ]
+  }
+};

3d2f1439e8fa515accfae4dfcdca8fd6c237f810
Forgetfull <oksana14888@gmail.com>
2020-05-05 11:25:00 +0500

    working webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('11')
+document.write('1')
+import "./react.js"
+import style from "./script.sass"
+//  import style from "./style.css"
+console.log("style" + style )
+
+a = b;
+
---
+++ src/react.js
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+render(<h1>react is working</h1>, document.getElementById("App"));
+console.log('react')

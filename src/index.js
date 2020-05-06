23febc638248dca58508ed12f4a991f1dca92d6e
f0rGetFull <f0rgetful@yandex.ru>
2020-05-06 23:47:30 +0300

    bingo!!slider 1.0 works

--- src/index.js
+++ src/index.js
@@ -4,6 +4,32 @@ import  "./style.sass"
 //  import style from "./style.css"
 
 
-//Задачи пиксельных опций через js
-//Работа от стрелок < >
-    // Работа от стрелок вверх вниз
+
+let slider = document.querySelector('.slider');
+let strip = document.querySelector('.strip');
+let left = document.querySelector('.button-left');
+let right = document.querySelector('.button-right');
+
+
+let stepLeft = 0;
+let stepRight = 0;
+function stepZero() {stepLeft = 0}
+function moveStripleft(){
+
+    stepLeft++;
+    if(stepLeft > 12){
+        stepZero()
+    }
+    console.log(stepLeft)
+    strip.style.left = -stepLeft*256 + 'px' 
+}
+
+function moveStripRight(){
+
+    stepRight++;
+    strip.style.left = stepRight*256 + 'px' 
+
+}
+
+left.addEventListener('click',moveStripleft)
+right.addEventListener('click',moveStripRight)

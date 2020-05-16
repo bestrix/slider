2d693398756914c6ace5e8bc27ae2d71e68559bd
f0rGetFull <f0rgetful@yandex.ru>
2020-05-16 10:04:32 +0300

    как создать много элементов автом

--- src/script.js
+++ src/script.js
@@ -1,4 +1,12 @@
+
 let strip = document.querySelector('.strip')
+
+//сканирование html 
+let massImgSrc = [];
+for(let item of strip.children){
+    massImgSrc.push(item.src)
+}
+
 
 //remove
 function removeImg(node,elements){
@@ -6,5 +14,21 @@ function removeImg(node,elements){
         node.children[0].remove()
     }
 }
+removeImg(strip,strip.children.length);
+function name(){
+    return 'alla'
+}
 
-removeImg(strip,strip.children.length)
+// создание изображений
+// for(let i = 0; i < strip.children.length; i++){
+//     let name = 'img' + i;
+//     let name  = document.createElement('img');
+//     name.src = massImgSrc[0];
+//     name.className = "lol"
+//     strip.appendChild(name)
+// }
+
+
+//генератор имени
+
+

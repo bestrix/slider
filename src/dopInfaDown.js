15c3d75b3dec6cca49e06ca4a45db8b26b3a28e9
f0rGetFull <f0rgetful@yandex.ru>
2020-06-02 20:38:06 +0300

    сделал появлегие инфо-окна посередине

--- src/dopInfaDown.js
+++ src/dopInfaDown.js
@@ -2,8 +2,7 @@ let strip = document.querySelector('.strip')
 let middle = strip.children[1];
 let windowIsMake = false;
 
-
-
+console.log(slider)
 window.addEventListener('keydown',(e) => {
     console.log(e.keyCode)
     if(e.keyCode == "40"){
@@ -19,7 +18,10 @@ function windowInfaCreate(){
         div.className = "windiws_info";
         div.id = 'winInfo';
         div.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
-        document.body.append(div);
+        slider.append(div);
+        console.log(slider.clientTop)
+        div.style.left = slider.clientWidth / 2 + div.clientWidth / 2 + 30  + 'px';
+        div.style.marginTop = slider.clientTop + 'px';
     }
     windowIsMake = true;
     plusOk = false;
